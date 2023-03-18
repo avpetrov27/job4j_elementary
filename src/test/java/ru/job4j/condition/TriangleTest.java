f62f51668b2c684790759df793d023d2b26ca229
avpetrov27 <avpetrov27@yandex.ru>
2023-03-18 10:06:35 +0300

    Чтение кода и базовые ошибки [#40205]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TriangleTest {
-
     @Test
     public void whenExist() {
         double ab = 2.0;
