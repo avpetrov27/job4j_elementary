db4b6bf40439b07a2cdeea1f28ea2915fd1fd0d5
avpetrov27 <avpetrov27@yandex.ru>
2023-03-19 13:47:55 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,78 @@ public class MaxTest {
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax5To6To7Then7() {
+        int first = 5;
+        int second = 6;
+        int third = 7;
+        int result = Max.max(first, second, third);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax10To6To7Then10() {
+        int first = 10;
+        int second = 6;
+        int third = 7;
+        int result = Max.max(first, second, third);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax10To16To7Then16() {
+        int first = 10;
+        int second = 16;
+        int third = 7;
+        int result = Max.max(first, second, third);
+        int expected = 16;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax10To16To7to8Then16() {
+        int first = 10;
+        int second = 16;
+        int third = 7;
+        int fourth = 8;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 16;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax20To16To7to8Then20() {
+        int first = 20;
+        int second = 16;
+        int third = 7;
+        int fourth = 8;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax0To0To7to8Then8() {
+        int first = 0;
+        int second = 0;
+        int third = 7;
+        int fourth = 8;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMaxMinus5To1To17to8Then17() {
+        int first = -5;
+        int second = 1;
+        int third = 17;
+        int fourth = 8;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 17;
+        assertThat(result).isEqualTo(expected);
+    }
 }
